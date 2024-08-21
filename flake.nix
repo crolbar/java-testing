1736a3a0dae70c257044d185b6d69c5d8bbf00e4
crolbar <crolbar@crolbar.com>
2024-08-21 11:03:32 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux"];
+    eachSystem = nixpkgs.lib.genAttrs systems;
+
+    pkgsFor = eachSystem (
+      system:
+        import nixpkgs {inherit system;}
+    );
+  in {
+    devShells = eachSystem (
+      system: let
+        pkgs = pkgsFor.${system};
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            jdk
+          ];
+        };
+      }
+    );
+
+    packages = eachSystem (
+      system: let
+        pkgs = pkgsFor.${system};
+
+        test = pkgs.stdenv.mkDerivation rec {
+          pname = "test";
+          version = "0.1";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            jdk
+          ];
+
+          dontPatch = true;
+          dontConfigure = true;
+
+          buildPhase = ''
+            javac Hello.java
+          '';
+
+          # The installPhase installs the compiled class into $out
+          installPhase = ''
+            mkdir -p $out/bin
+            mv Hello.class $out/bin
+
+            echo "#!/bin/sh" > $out/bin/${pname}
+            echo "${pkgs.jdk}/bin/java -cp $out/bin Hello" >> $out/bin/${pname}
+            chmod +x $out/bin/${pname}
+          '';
+        };
+      in {
+        inherit test;
+        default = test;
+      }
+    );
+  };
+}

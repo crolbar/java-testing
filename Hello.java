96f9fa925e7de8270e566a5bf748975cad73f32a
crolbar <crolbar@crolbar.com>
2024-08-21 15:48:36 +0300

    tmp

--- Hello.java
+++ Hello.java
@@ -1,7 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
-        for (int i = 0; i <= 50; ++i) {
-            System.out.println("hello: " + i);
+        var num = 2;
+        String str = "my str";
+        byte b = (byte) 126;
+
+        System.out.println(String.format("num: %d, str: %s, b: %d", num, str.charAt(0), b));
+
+        for (int i = 0; i < str.length(); i++) {
+            b = (byte) str.charAt(i);
+            System.out.println(String.format("n: %d, b: %d, c: '%d'", i, b, (char) b));
         }
     }
 }

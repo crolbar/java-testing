59bb9173327ea778e43a5c4463a6b6e9066ce406
crolbar <crolbar@crolbar.com>
2024-10-20 10:06:01 +0300

    leetcode.BackTracking: 39 remove unneeded

--- src/leetcode/BackTracking/_39.java
+++ src/leetcode/BackTracking/_39.java
@@ -7,13 +7,7 @@ public class _39 {
     void r(int[] nums, int target, List<List<Integer>> res, ArrayList<Integer> list, int i) {
 
         list.add(nums[i]);
-
         target -= nums[i];
-
-        if (i > nums.length) {
-            list.removeLast();
-            return;
-        }
 
         if (target < 0) {
             list.removeLast();
@@ -27,10 +21,7 @@ public class _39 {
         }
 
         while (i < nums.length) {
-            System.out.printf("i: %d, target(%d) - nums[i](%d): %d, list: %s\n", i, target, nums[i], target - nums[i], list.toString());
-
             r(nums, target, res, list, i);
-
             i++;
         }
 
@@ -39,10 +30,12 @@ public class _39 {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
+
         ArrayList<Integer> l = new ArrayList<>();
         for (int i = 0; i < candidates.length; i++) {
             r(candidates, target, res, l, i);
         }
+
         return res;
     }
 }

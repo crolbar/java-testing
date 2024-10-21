6772c71bca4692099b721f5c5534c67554255799
crolbar <crolbar@crolbar.com>
2024-10-21 10:03:09 +0300

    leetcode.BackTracking: 46 use int as lookup instead of hashSet

--- src/leetcode/BackTracking/_46.java
+++ src/leetcode/BackTracking/_46.java
@@ -1,27 +1,21 @@
 package leetcode.BackTracking;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class _46 {
-    void r(int[] nums, List<List<Integer>> res, ArrayList<Integer> l, HashSet<Integer> set) {
-        if (l.size() == nums.length) {
+    void r(int[] nums, List<List<Integer>> res, ArrayList<Integer> l, int bits) {
+        if (Integer.bitCount(bits) == nums.length) {
             res.add(new ArrayList<>(l));
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i]))
+        for (int i = 0, b = 1; i < nums.length; i++, b *= 2) {
+            if ((bits & b) > 0)
                 continue;
 
-
             l.add(nums[i]);
-            set.add(nums[i]);
-
-            r(nums, res, l, set);
-
-            set.remove(nums[i]);
+            r(nums, res, l, bits | b);
             l.removeLast();
         }
     }
@@ -30,7 +24,7 @@ public class _46 {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
 
-        r(nums, res, new ArrayList<>(), new HashSet<>());
+        r(nums, res, new ArrayList<>(), 0);
 
         return res;
     }

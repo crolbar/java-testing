f32facd61180a212005a89601743ea67acd754b5
crolbar <crolbar@crolbar.com>
2024-11-12 09:54:18 +0200

    leetcode.Graph: a bit better 684

--- src/leetcode/Graph/_684.java
+++ src/leetcode/Graph/_684.java
@@ -10,14 +10,10 @@ class _684
     boolean dfs(HashMap<Integer, ArrayList<Integer>> ed,
                 HashSet<Integer> seen,
                 int i,
-                int p,
-                int start)
+                int p)
     {
-        if (i == start && p != 0)
-            return true;
-
         if (seen.contains(i))
-            return false;
+            return true;
 
         seen.add(i);
 
@@ -25,9 +21,8 @@ class _684
             if (p == nei)
                 continue;
 
-            if (dfs(ed, seen, nei, i, start)) {
+            if (dfs(ed, seen, nei, i))
                 return true;
-            }
         }
 
         return false;
@@ -37,21 +32,16 @@ class _684
     int[] findRedundantConnection(int[][] edges)
     {
         HashMap<Integer, ArrayList<Integer>> ed = new HashMap<>();
+
         for (int[] edge : edges) {
             ed.putIfAbsent(edge[0], new ArrayList<>());
             ed.putIfAbsent(edge[1], new ArrayList<>());
 
             ed.get(edge[0]).add(edge[1]);
             ed.get(edge[1]).add(edge[0]);
-        }
 
-        for (int i = edges.length - 1; i >= 0; i--) {
-            int[] edge = edges[i];
-
-            if (dfs(ed, new HashSet<>(), edge[0], 0, edge[0]) &&
-                dfs(ed, new HashSet<>(), edge[1], 0, edge[1])) {
+            if (dfs(ed, new HashSet<>(), edge[0], 0))
                 return edge;
-            }
         }
 
         return new int[]{ 0 };

398522dd4db90a0ba4ab87ca41c2b8ad92683438
crolbar <crolbar@crolbar.com>
2024-11-14 10:27:31 +0200

    leetcode.Graph: dsu 323

--- src/leetcode/Graph/_323.java
+++ src/leetcode/Graph/_323.java
@@ -1,52 +1,50 @@
 package leetcode.Graph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-
 public
 class _323
 {
-    void dfs(HashMap<Integer, List<Integer>> map,
-             HashSet<Integer> visited,
-             int curr)
-    {
-        visited.add(curr);
+    int find(int[] par, int v) {
+        if (par[v] != v)
+            par[v] = find(par, par[v]);
+        return par[v];
+    }
 
-        for (int nei : map.get(curr)) {
-            if (!visited.contains(nei))
-                dfs(map, visited, nei);
+    boolean union(int[] par, int[] rank, int u, int v) {
+        int parU = find(par, u);
+        int parV = find(par, v);
+
+        if (parU == parV)
+            return false;
+
+        if (rank[parU] > rank[parV]) {
+            par[parV] = parU;
+        } else if (rank[parU] < rank[parV]) {
+            par[parU] = parV;
+        } else {
+            par[parU] = parV;
+            rank[parV]++;
         }
+
+        return true;
     }
 
   public
     int countComponents(int n, int[][] edges)
     {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        HashSet<Integer> visited = new HashSet<>();
+
+        int[] par   = new int[n];
+        int[] rank  = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            par[i]  = i;
+            rank[i] = 1;
+        }
 
         for (int[] edge : edges) {
-            map.putIfAbsent(edge[0], new ArrayList<>());
-            map.putIfAbsent(edge[1], new ArrayList<>());
-
-            map.get(edge[0]).add(edge[1]);
-            map.get(edge[1]).add(edge[0]);
+            if (union(par, rank, edge[0], edge[1]))
+                n--;
         }
 
-        int c = 0;
-        for (int i = 0; i < n; i++) {
-            if (visited.contains(i))
-                continue;
-
-            c++;
-
-            if (!map.containsKey(i))
-                continue;
-
-            dfs(map, visited, i);
-        }
-
-        return c;
+        return n;
     }
 }

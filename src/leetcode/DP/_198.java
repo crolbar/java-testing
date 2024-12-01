a7547b63d215c5e1890bf652734bf68da321aea8
crolbar <crolbar@crolbar.com>
2024-12-01 09:26:44 +0200

    leetcode.DP: ops 198

--- src/leetcode/DP/_198.java
+++ src/leetcode/DP/_198.java
@@ -14,7 +14,6 @@ class _198
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
-        Math.max(2, 3);
 
         for (int i = 2; i < nums.length; i++)
             dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);

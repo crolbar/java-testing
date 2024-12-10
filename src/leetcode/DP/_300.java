0f84744563cfc94feec50dc5407a6596213ab59e
crolbar <crolbar@crolbar.com>
2024-12-10 12:38:51 +0200

    leetcode.DP: 300 (bottom up)

--- src/leetcode/DP/_300.java
+++ src/leetcode/DP/_300.java
@@ -5,40 +5,23 @@ import java.util.Arrays;
 public
 class _300
 {
-  private
-    int dfs(int[] nums, int prevI, int i, int[] memo)
-    {
-        if (i >= nums.length)
-            return 1;
-
-        if (nums[prevI] >= nums[i])
-            return dfs(nums, prevI, i + 1, memo);
-
-        int countCurr =
-          (memo[i] != -1) ? 1 + memo[i] : 1 + dfs(nums, i, i + 1, memo);
-
-        int skipCurr = dfs(nums, prevI, i + 1, memo);
-
-        int c = Math.max(countCurr, skipCurr);
-
-        memo[prevI] = c;
-
-        return c;
-    }
-
   public
     int lengthOfLIS(int[] nums)
     {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = nums.length - 1; i >= 0; i--)
+            for (int j = i + 1; j < nums.length; j++)
+                if (nums[i] < nums[j])
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+
+        System.out.println(Arrays.toString(dp));
+
         int res = 0;
-        int[] memo = new int[nums.length];
-        Arrays.fill(memo, -1);
 
-        for (int i = 0; i < nums.length; i++) {
-            int c = dfs(nums, i, i + 1, memo);
-            res = Math.max(res, c);
-        }
-
-        System.out.println(Arrays.toString(memo));
+        for (int i = 0; i < dp.length; i++)
+            res = Math.max(res, dp[i]);
 
         return res;
     }
